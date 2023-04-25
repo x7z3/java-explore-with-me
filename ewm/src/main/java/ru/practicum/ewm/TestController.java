e537e5f1b381bd3a72b8df5bfce1e0889aac0d16
Sayid Umarsayidov <x7z3@mail.ru>
2023-04-25 23:17:32 +0300

    base ewm

---
+++ ewm/src/main/java/ru/practicum/ewm/TestController.java
@@ -0,0 +1,24 @@
+package ru.practicum.ewm;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.statistics.client.StatisticsRestClient;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "")
+public class TestController {
+    private final StatisticsRestClient statsClient;
+
+    @GetMapping("/test")
+    public ResponseEntity<Void> test(HttpServletRequest request) {
+        statsClient.hit(request);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
